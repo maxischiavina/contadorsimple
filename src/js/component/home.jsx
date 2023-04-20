19fa7b13f0558b743ee5883604d81961c41e31af
maxischiavina <maxischiavina@hotmail.com>
2023-04-20 08:51:35 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,55 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	console.log(props)
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center d-flex justify-content-center bg-dark">
+
+<div className="card bg-dark">
+  <div className="card-body">
+  <div className="text-white me-4  fs-1"><i className="fa-regular fa-clock"></i></div>
+	</div>
+	</div>
+
+	<div className="card bg-dark">
+  <div className="card-body">
+  <div className="text-white text-center me-4 border-dark fs-1">{props.cienmil}</div>
+	</div>
+	</div>
+
+	
+	<div className="card bg-dark">
+  <div className="card-body">
+  <div className="text-white me-4 border-white fs-1">{props.diezmil}</div>
+	</div>
+	</div>
+
+	<div className="card bg-dark">
+  <div className="card-body">
+  <div className="text-white me-4 border-white fs-1">{props.mil}</div>
+	</div>
+	</div>
+
+	<div className="card bg-dark">
+  <div className="card-body">
+  <div className="text-white me-4 border-white fs-1">{props.centena}</div>
+	</div>
+	</div>	
+
+	<div className="card bg-dark">
+  <div className="card-body">
+  <div className="text-white me-4 border-white fs-1">{props.decena}</div>
+	</div>
+	</div>	
+
+	<div className="card bg-dark">
+  <div className="card-body">
+  <div className="text-white me-4 border-white fs-1">{props.unidad}</div>
+	</div>
+	</div>
+
 		</div>
-	);
-};
+	);}
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,61 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let num = 0
+let num2 = 0
+let num3 = 0
+let num4 = 0
+let num5 = 0
+let num6 = 0
+
+
+setInterval(function() {
+            num++;
+            if (num === 10) {
+                num = 0;
+                num2++;
+            }
+            if (num2 === 10) {
+                num2 = 0;
+                num3++
+            }
+            if (num3 === 10) {
+                num3 = 0;
+                num4++
+            }
+            if (num4 === 6) {
+                num4 = 0;
+                num5++
+            }
+            if (num5 === 10) {
+                num5 = 0;
+                num6++
+            }
+            if (num6 === 2 && num5 === 4) {
+                num = 0;
+                num2 = 0;
+                num3 = 0;
+                num4 = 0;
+                num5 = 0;
+                num6 = 0
+            }
+            ReactDOM.render( < Home unidad = {
+                    num
+                }
+                decena = {
+                    num2
+                }
+                centena = {
+                    num3
+                }
+                mil = {
+                    num4
+                }
+                diezmil = {
+                    num5
+                }
+                cienmil = {
+                    num6
+                }
+                />, document.querySelector("#app"));
+            }, 0.0001)
