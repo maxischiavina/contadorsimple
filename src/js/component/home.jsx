8059996d595d68a2e4e6dceb30d7cb9ecc5a280e
maxischiavina <maxischiavina@hotmail.com>
2023-04-21 09:05:33 +0000

    tercer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -17,38 +17,38 @@ const Home = (props) => {
 
 	<div className="card bg-dark">
   <div className="card-body">
-  <div className="text-white text-center me-4 border-dark fs-1">{props.cienmil}</div>
+  <div className="text-white text-center me-4 border-dark fs-1 text-center w-100">{props.cienmil}</div>
 	</div>
 	</div>
 
 	
 	<div className="card bg-dark">
   <div className="card-body">
-  <div className="text-white me-4 border-white fs-1">{props.diezmil}</div>
+  <div className="text-white me-4 border-white fs-1 text-center w-100">{props.diezmil}</div>
 	</div>
 	</div>
 
 	<div className="card bg-dark">
   <div className="card-body">
-  <div className="text-white me-4 border-white fs-1">{props.mil}</div>
+  <div className="text-white me-4 border-white fs-1 text-center w-100">{props.mil}</div>
 	</div>
 	</div>
 
 	<div className="card bg-dark">
   <div className="card-body">
-  <div className="text-white me-4 border-white fs-1">{props.centena}</div>
+  <div className="text-white me-4 border-white fs-1 text-center w-100">{props.centena}</div>
 	</div>
 	</div>	
 
 	<div className="card bg-dark">
   <div className="card-body">
-  <div className="text-white me-4 border-white fs-1">{props.decena}</div>
+  <div className="text-white me-4 border-white fs-1 text-center w-100">{props.decena}</div>
 	</div>
 	</div>	
 
 	<div className="card bg-dark">
   <div className="card-body">
-  <div className="text-white me-4 border-white fs-1">{props.unidad}</div>
+  <div className="text-white me-4 border-white fs-1 text-center w-100">{props.unidad}</div>
 	</div>
 	</div>
 
